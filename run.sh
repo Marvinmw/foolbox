ebf00de629e74749d1d40c5edca0903f951b6499
Wei MA <wei.ma@uni.lu>
2019-07-01 11:46:42 +0200

    model loaded

--- run.sh
+++ run.sh
@@ -10,6 +10,13 @@
 #SBATCH --mail-type=end,fail
 #SBATCH --mail-user=wei.ma@uni.lu
 
+
+module purge
+module load math/Keras/2.1.6-foss-2018a-TensorFlow-1.8.0-Python-3.6.4
+module load lib/TensorFlow/1.8.0-foss-2018a-Python-3.6.4-CUDA-9.1.85
+module load vis/matplotlib/2.1.2-foss-2018a-Python-3.6.4
+pip install --upgrade --user  keras sklearn tqdm
+
 srun -n 1 --pty python ./generateAdvImgs.py -m lenet -d mnist -a FGSM CW > ./deepxplore_f.info
 srun -n 1 --pty python ./generateAdvImgs.py -m deepxplore -d fashion_mnist -a FGSM CW > ./deepxplore_f.info
 srun -n 1 --pty python ./generateAdvImgs.py -m vgg -d cifar10 -a FGSM CW > ./vgg.info
