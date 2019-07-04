96f332b8938df2a7cba2bbc61658113f975b22bb
Wei MA <wei.ma@uni.lu>
2019-07-04 17:32:56 +0200

    s

--- summaryData.py
+++ summaryData.py
@@ -2,7 +2,7 @@ import numpy as np
 import natsort
 import os
 from tqdm import tqdm
-def summaryCW(modellist =['mlp','lenet','deepxplore'], datalist = ['mnist','fashion_mnist'], adv=['CW', 'FGSM']):
+def summaryCW(modellist =['mlp','lenet','deepxplore'], datalist = ['mnist','fashion_mnist'], adv=['CW']):
     #modellist =['mlp','lenet','deepxplore', 'netinnet','vgg']
     #datalist = ['mnist','fashion_mnist']
     for m in modellist:
@@ -27,6 +27,8 @@ def summaryCW(modellist =['mlp','lenet','deepxplore'], datalist = ['mnist','fash
                                                    images  = np.load(fpath)
                                                    #print(images.shape)
                                                    total_images.extend(list(images))
+                                               finalimg = np.load(os.path.join(path, 'best.npy')).items()
+                                               total_images.append(finalimg.get('img'))
                                                np.save(svaedpath, total_images)
                                                break
                                            else:
@@ -37,8 +39,42 @@ def summaryCW(modellist =['mlp','lenet','deepxplore'], datalist = ['mnist','fash
 
                     dowork()
 
+def summaryFGSM(modellist =['mlp','lenet','deepxplore'], datalist = ['mnist','fashion_mnist'], adv=['FGSM']):
+    #modellist =['mlp','lenet','deepxplore', 'netinnet','vgg']
+    #datalist = ['mnist','fashion_mnist']
+    for m in modellist:
+        for d in datalist:
+            for a in adv:
+                if not os.path.isdir('m_%s_%s_%s'%(a, m,d)):
+                    os.mkdir('m_%s_%s_%s'%(a,m,d))
+                for j in tqdm(np.arange(300)):
+                    def dowork():
+                            path = "%s_%s_%s/%d/%d"%(a, m,d,j)
+                            svaedpath = "m_%s_%s_%s/%d.npy" % (a,m, d, j)
+                            if os.path.isdir(path):
+                                    if len(os.listdir(path))!=0:
+                                           files = os.listdir(path)
+                                           if 'found.npy' in files:
+                                               files.remove('found.npy')
+                                               sf = natsort.natsorted(files)
+                                               total_images = []
+                                               for f in sf:
+                                                   fpath = os.path.join(path, f)
+                                                   images  = np.load(fpath)
+                                                   #print(images.shape)
+                                                   total_images.extend(list(images))
+                                               finalimg = np.load(os.path.join(path, 'found.npy')).items()
+                                               total_images.append(finalimg.get('img'))
+                                               np.save(svaedpath, total_images)
 
+
+
+                    dowork()
 if __name__ == '__main__':
-    summaryCW(modellist=['lenet', 'deepxplore'], datalist=['mnist', 'fashion_mnist'], adv=['FGSM'])
-    summaryCW(modellist=['vgg'], datalist=['cifar10'], adv=['FGSM'])
+    summaryCW(modellist=['lenet', 'deepxplore'], datalist=['mnist', 'fashion_mnist'])
+    summaryCW(modellist=['vgg'], datalist=['cifar10'])
     summaryCW(modellist=['netinnet'], datalist=['cifar10'])
+
+    summaryFGSM(modellist=['lenet', 'deepxplore'], datalist=['mnist', 'fashion_mnist'])
+    summaryFGSM(modellist=['vgg'], datalist=['cifar10'])
+    summaryFGSM(modellist=['netinnet'], datalist=['cifar10'])
