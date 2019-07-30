fa10f182d6e43f157569a45a0c5d735475b31fc5
Wei MA <wei.ma@uni.lu>
2019-07-30 09:28:57 +0200

    mlp

--- summaryData.py
+++ summaryData.py
@@ -85,10 +85,12 @@ def summaryFGSM(modellist =['mlp','lenet','deepxplore'], datalist = ['mnist','fa
 
                     dowork()
 if __name__ == '__main__':
-    summaryCW(modellist=['lenet', 'deepxplore'], datalist=['mnist', 'fashion_mnist'])
-    summaryCW(modellist=['vgg'], datalist=['cifar10'])
-    summaryCW(modellist=['netinnet'], datalist=['cifar10'])
+    summaryCW(modellist=['mlp'], datalist=['mnist', 'fashion_mnist'])
+    #summaryCW(modellist=['lenet', 'deepxplore'], datalist=['mnist', 'fashion_mnist'])
+    #summaryCW(modellist=['vgg'], datalist=['cifar10'])
+    #summaryCW(modellist=['netinnet'], datalist=['cifar10'])
 
-    summaryFGSM(modellist=['lenet', 'deepxplore'], datalist=['mnist', 'fashion_mnist'])
-    summaryFGSM(modellist=['vgg'], datalist=['cifar10'])
-    summaryFGSM(modellist=['netinnet'], datalist=['cifar10'])
+    summaryFGSM(modellist=['mlp'], datalist=['mnist', 'fashion_mnist'])
+    #summaryFGSM(modellist=['lenet', 'deepxplore'], datalist=['mnist', 'fashion_mnist'])
+    #summaryFGSM(modellist=['vgg'], datalist=['cifar10'])
+    #summaryFGSM(modellist=['netinnet'], datalist=['cifar10'])
