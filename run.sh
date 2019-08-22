686866037874a227f53417a2adcd60f272a78b61
Wei MA <wei.ma@uni.lu>
2019-08-22 15:06:06 +0200

    script

--- run.sh
+++ run.sh
@@ -15,15 +15,15 @@ module purge
 module  load lib/TensorFlow/1.8.0-foss-2018a-Python-3.6.4
 module load math/Keras/2.1.6-foss-2018a-TensorFlow-1.8.0-Python-3.6.4
 pip install --upgrade --user  keras sklearn tqdm
-
-srun -n 1 --pty python ./generateAdvImgs.py -m lenet -d mnist -a FGSM CW > ./deepxplore_f.info
-srun -n 1 --pty python ./generateAdvImgs.py -m deepxplore -d fashion_mnist -a FGSM CW > ./deepxplore_f.info
-srun -n 1 --pty python ./generateAdvImgs.py -m vgg -d cifar10 -a FGSM CW > ./vgg.info
-
-srun -n 1 --pty python ./generateAdvImgs.py -m netinnet -d cifar10 -a FGSM CW > ./netinnet.info
-srun -n 1 --pty python ./generateAdvImgs.py -m mlp -d mnist -a FGSM CW > ./mlp_m.info
-srun -n 1 --pty python ./generateAdvImgs.py -m mlp -d fashion_mnist -a FGSM CW > ./mlp_f.info
-
-srun -n 1 --pty python ./generateAdvImgs.py -m lenet -d fashion_mnist -a FGSM CW > ./lenet_f.info
-
-srun -n 1 --pty python ./generateAdvImgs.py -m deepxplore -d mnist -a FGSM CW > ./deepxplore_m.info
+python summaryData.py
+#srun -n 1 --pty python ./generateAdvImgs.py -m lenet -d mnist -a FGSM CW > ./deepxplore_f.info
+#srun -n 1 --pty python ./generateAdvImgs.py -m deepxplore -d fashion_mnist -a FGSM CW > ./deepxplore_f.info
+#srun -n 1 --pty python ./generateAdvImgs.py -m vgg -d cifar10 -a FGSM CW > ./vgg.info
+#
+#srun -n 1 --pty python ./generateAdvImgs.py -m netinnet -d cifar10 -a FGSM CW > ./netinnet.info
+#srun -n 1 --pty python ./generateAdvImgs.py -m mlp -d mnist -a FGSM CW > ./mlp_m.info
+#srun -n 1 --pty python ./generateAdvImgs.py -m mlp -d fashion_mnist -a FGSM CW > ./mlp_f.info
+#
+#srun -n 1 --pty python ./generateAdvImgs.py -m lenet -d fashion_mnist -a FGSM CW > ./lenet_f.info
+#
+#srun -n 1 --pty python ./generateAdvImgs.py -m deepxplore -d mnist -a FGSM CW > ./deepxplore_m.info
